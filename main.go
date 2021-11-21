8d06443fc0569752a153acee8e16f0f8604add62
bonivan <ivan.bondarev@myemail.com>
2021-11-20 21:16:55 -0500

    Initial commit

--- main.go
+++ main.go
@@ -1,5 +1,61 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os"
+	"regexp"
 
+	"github.com/google/go-github/v40/github"
+	"github.com/posener/goaction"
+	log "github.com/sirupsen/logrus"
+)
+
+var token = os.Getenv("INPUT_GITHUB-TOKEN")
+
+func main() {
+	titleRegex := os.Getenv("INPUT_TITLE-REGEX")
+	errorMessage := os.Getenv("INPUT_ERROR-MESSAGE")
+	excludeFilesRegex := os.Getenv("INPUT_EXCLUDE-FILES-REGEX")
+	//titleRegex := "^(\\[[a-z ]*\\]|Bump) "
+	//errorMessage := ""
+	//excludeFilesRegex := ".*(\\.gradle|\\.java|\\.xml).*"
+
+	ctx := context.Background()
+	client := github.NewClient(nil)
+
+	pr, err := goaction.GetPullRequest()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doTitleCheck := true
+	if excludeFilesRegex != "" {
+		doTitleCheck = false
+		files, resp, err := client.PullRequests.ListFiles(ctx, pr.GetOrganization().GetName(), pr.GetRepo().GetName(), pr.GetNumber(), nil)
+		if err != nil || resp.StatusCode != 200 {
+			log.Fatalf("Cannot get files for PR. %v", err)
+		}
+		for _, file := range files {
+			match, err := regexp.MatchString(excludeFilesRegex, file.GetFilename())
+			if err != nil {
+				log.Fatal(err)
+			}
+			if !match {
+				doTitleCheck = true
+				break
+			}
+		}
+	}
+	if doTitleCheck {
+		match, err := regexp.MatchString(titleRegex, pr.GetPullRequest().GetTitle())
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !match {
+			if errorMessage == "" {
+				errorMessage = "Please include JIRA issue ID at the beginning of PR title"
+			}
+			log.Fatalf(errorMessage)
+		}
+	}
 }
